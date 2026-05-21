c83a2e7532a8cf08e09187dfd8412ce3e7aa116e
skebob1717 <mtimofey188@gmail.com>
2026-05-21 19:19:30 +0300

    Update delete_books.py

--- delete_books.py
+++ delete_books.py
@@ -1,1 +1,20 @@
-print('Книга удалена!')
+def delete_book():
+    books = load_books()
+    if not books:
+        print("Нет книг для удаления.")
+        return
+    
+    print("\n=== Список книг для удаления ===")
+    for i, book in enumerate(books, 1):
+        print(f"{i}. {book['author']} - {book['title']}")
+    
+    try:
+        index = int(input("Введите номер книги для удаления: ")) - 1
+        if 0 <= index < len(books):
+            removed = books.pop(index)
+            save_books(books)
+            print(f"Книга '{removed['title']}' удалена!")
+        else:
+            print("Неверный номер.")
+    except ValueError:
+        print("Введите число.")
